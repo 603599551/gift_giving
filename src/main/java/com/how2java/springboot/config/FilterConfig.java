f73a1e0813af74a6d285afbabc7a396c15d3f667
zhanjinqi <603599551@qq.com>
2018-12-09 12:27:39 +0800

    配置过滤器顺序：1.处理中文乱码 。2.解决跨域访问

--- src/main/java/com/how2java/springboot/config/FilterConfig.java
+++ src/main/java/com/how2java/springboot/config/FilterConfig.java
@@ -1,33 +1,33 @@
 package com.how2java.springboot.config;
 
 
-import com.how2java.springboot.filter.AFilter;
-import com.how2java.springboot.filter.BFilter;
+import com.how2java.springboot.filter.CharactorFilter;
+import com.how2java.springboot.filter.CrossDomainAccessFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * @Description
- * @Author zjq
- * @Date 2018/10/27 18:47
+ * 配置过滤器顺序
+ * @Author CaryZ
+ * @Date 2018-12-09
  */
 @Configuration
 public class FilterConfig {
     @Bean
-    public FilterRegistrationBean buildLoginFilter(){
-        FilterRegistrationBean filterRegistrationBean=new FilterRegistrationBean(new BFilter());
+    public FilterRegistrationBean buildCharactorFilter(){
+        FilterRegistrationBean filterRegistrationBean=new FilterRegistrationBean(new CrossDomainAccessFilter());
         filterRegistrationBean.setOrder(1);
-        filterRegistrationBean.setName("BFilter");
+        filterRegistrationBean.setName("CharactorFilter");
         filterRegistrationBean.addUrlPatterns("/*");
         return filterRegistrationBean;
     }
 
     @Bean
-    public FilterRegistrationBean buildAFilter(){
-        FilterRegistrationBean filterRegistrationBean=new FilterRegistrationBean(new AFilter());
+    public FilterRegistrationBean buildCrossDomainAccessFilter(){
+        FilterRegistrationBean filterRegistrationBean=new FilterRegistrationBean(new CharactorFilter());
         filterRegistrationBean.setOrder(2);
-        filterRegistrationBean.setName("AFilter");
+        filterRegistrationBean.setName("CrossDomainAccessFilter");
         filterRegistrationBean.addUrlPatterns("/*");
         return filterRegistrationBean;
     }
